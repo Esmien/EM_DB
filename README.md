0fc9c3bd881bd0bb7a60bcbf93089bb619e91d01
Владислав Сороковых <black.walf16@gmail.com>
2026-03-17 14:07:19 +0300

    finish

---
+++ README.md
@@ -0,0 +1,21 @@
+Подготовка окружения:
+
+```cp .env.example .env```
+
+Запуск:
+
+```docker compose up --build -d```
+
+
+Проверка:
+http://localhost:8080
+
+СУБД: PostgeSQL
+
+Сервер: `db`
+
+Логин: `admin`
+
+Пароль: `admin`
+
+БД: `em`
--- app/config.py
+++ app/config.py
@@ -7,8 +7,8 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
-    POSTGRES_HOST: str = "localhost"
-    POSTGRES_PORT: int = 5432
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
 
 
     @computed_field
